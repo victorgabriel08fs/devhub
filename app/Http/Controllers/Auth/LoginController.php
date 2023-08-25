a76872b6382b41349dae48989491916232850659
Victor Gabriel <victorgabriel08fs@gmail.com>
2023-08-25 12:39:42 -0300

    importando repositórios públicos

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Http;
 
 class LoginController extends Controller
 {
@@ -60,7 +62,7 @@ class LoginController extends Controller
      */
     public function handleProviderCallback($provider)
     {
-        $user = Socialite::driver($provider)->scopes(['read:user', 'public_repo'])->user();
+        $user = Socialite::driver($provider)->scopes(['read:user'])->user();
         $authUser = $this->findOrCreateUser($user, $provider);
         Auth::login($authUser, true);
         return redirect($this->redirectTo);
@@ -75,16 +77,44 @@ class LoginController extends Controller
      */
     public function findOrCreateUser($user, $provider)
     {
-        $authUser = User::where('provider_id', $user->id)->first();
-        if ($authUser) {
-            return $authUser;
+        try {
+            $authUser = User::where('provider_id', $user->id)->first();
+            if ($authUser) {
+                return $authUser;
+            }
+            $user = User::create([
+                'name'     => $user->name,
+                'email'    => $user->email,
+                'provider' => $provider,
+                'provider_id' => $user->id,
+                'username' => $user->nickname
+            ]);
+
+            $response = Http::get('https://api.github.com/users/' . $user->username . '/repos');
+            $data = $response->json();
+
+            function formatSlug($name)
+            {
+                $slug = strtolower($name);
+                $slug = str_replace(' ', '_', $slug);
+                return $slug;
+            }
+
+            foreach ($data as $item) {
+                Project::create([
+                    'user_id' => $user->id,
+                    'name' => str_replace('_', ' ', ucwords($item['name'])),
+                    'slug' => formatSlug(ucwords($item['name'])),
+                    'github_link' => $item['html_url'],
+                    'url' => !str_contains('http', $item['homepage']) ? 'https://www.' . $item['homepage'] : $item['homepage'],
+                    'visibility' => 'Public',
+                    'started_at' => $item['pushed_at']
+                ]);
+            }
+
+            return $user;
+        } catch (\Throwable $th) {
+            return redirect()->route('login');
         }
-        return User::create([
-            'name'     => $user->name,
-            'email'    => $user->email,
-            'provider' => $provider,
-            'provider_id' => $user->id,
-            'username' => $user->nickname
-        ]);
     }
 }
